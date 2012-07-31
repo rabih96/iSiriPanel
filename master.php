65be81041986d437ed3385a7a56c20ce3802b38f
Rabih <rabihmteyrek@hotmail.com>
2012-07-31 08:05:29 -0300

    Update master.php

--- master.php
+++ master.php
@@ -1,7 +1,7 @@
 <?php
 include('Net/SSH2.php');
 
-$ssh = new Net_SSH2('YOUR_IP_ADRESS'); 
+$ssh = new Net_SSH2('YOUR_IP_ADDRESS'); 
 if (!$ssh->login('root', 'YOUR_PASSWORD')) { 
     exit('Login Failed');
 }
